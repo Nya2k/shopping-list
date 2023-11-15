5fe3f12e6fb78036bc3fcf19f28c847238e86edf
Nya2k <eudoravanya@gmail.com>
2023-11-15 14:19:59 +0700

    tutorial 8

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, register, login_user, create_product, edit_product, delete_product, show_xml, show_json, get_product_json, add_product_ajax, logout_user
+from main.views import show_main, register, login_user, create_product, edit_product, delete_product, show_xml, show_json, get_product_json, add_product_ajax, logout_user, create_product_flutter
 
 app_name = 'main'
 
@@ -14,5 +14,6 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
-    path('logout/', logout_user, name="logout")
+    path('logout/', logout_user, name="logout"),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 import datetime
+import json
 from django.shortcuts import render, redirect
-from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse, JsonResponse
 from django.urls import reverse
 from django.core import serializers
 from django.contrib import messages
@@ -120,3 +121,22 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
